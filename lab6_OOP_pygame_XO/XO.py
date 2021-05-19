d3c503078ef8cc85d475d2354ef4688aecdf25a8
artemschirov <artemschirov@gmail.com>
2021-05-20 01:53:11 +0300

    Check and get click coords and check which cell was clicked + Background color + Documentation

--- lab6_OOP_pygame_XO/XO.py
+++ lab6_OOP_pygame_XO/XO.py
@@ -2,17 +2,18 @@ import pygame as pg
 from enum import Enum
 
 FPS = 60
-CELL_SIZE = 50
+CELL_SIZE = 100
 
 
 class Player:
     """
-
+    Create players with their unique input parameters.
+    Contain all info and abilities of players
     """
-
-    def __init__(self, name, cell):
+    def __init__(self, name: str, cell):
         """
-        Constructor of class Player
+        Constructor of class Player.
+        Build info math and graphical models of player
 
         :param name: string with name of new Player
         :param cell: Is the player playing crosses or zero?
@@ -34,11 +35,10 @@ class GameField:
     """
     Math model of field
     """
-
     def __init__(self):
         """
         Constructor of class GameField.
-        Build info model of game field
+        Build math model of field
         """
         self.height = 3
         self.width = 3
@@ -46,7 +46,9 @@ class GameField:
 
     def is_game_over(self):
         """
+        Check if someone win
 
+        :return: if win return True
         """
         pass
 
@@ -63,8 +65,11 @@ class GameFieldView:
     Load cell states.
     Determines the location of the click and which cell on click coords
     """
-
     def __init__(self, field):
+        """
+        Constructor of class GameFieldView.
+        Build graphical model of field
+        """
         self._field = field
         self._height = field.height * CELL_SIZE
         self._width = field.width * CELL_SIZE
@@ -79,23 +84,35 @@ class GameFieldView:
         """
 
         """
-        return True  # TODO: self._height учесть
+        if x in range(self._height)  \
+                and y in range(self._width):
+            print('True')
+            return True
+        else:
+            print('False')
+            return False
 
     def get_coords(self, x, y):
         """
 
-        :param x, y:
-        :return: which cell on click coords
+        :param x:
+        :param y:
+        :return: number of cell of the field according to click coords
         """
-        return 0, 0  # TODO: Реально вычислить
+        cell_x = x // CELL_SIZE  # height
+        cell_y = y // CELL_SIZE  # width
+
+        return cell_x, cell_y
 
 
 class GameRoundManager:
     """
-    Main cycle of game
+    Main cycle activate all parts of game
     """
-
     def __init__(self, player1: Player, player2: Player):
+        """
+        Constructor of class GameRoundManager
+        """
         self._players = [player1, player2]
         self._current_player = 0
         self.field = GameField()
@@ -105,7 +122,15 @@ class GameRoundManager:
 
         """
         player = self._players[self._current_player]
-        print("click handled", i, j)
+        self._current_player ^= 1
+        print("click handled", i, j, self._current_player)
+
+    def main_loop(self):
+        """
+
+        """
+        round_on = True
+        while round_on:
 
 
 class GameWindow:
@@ -114,38 +139,45 @@ class GameWindow:
     Includes field widget
     and game round manager
     """
-
     def __init__(self):
         """
-
+        Constructor of class GameWindow
         """
         pg.init()
-
-        self._width = 800
-        self._height = 600
-        self._title = 'XO OOP'
-        self._screen = pg.display.set_mode((self._width, self._height))
-        pg.display.set_caption(self._title)
 
         player1 = Player("God", Cell.ZERO)
         player2 = Player("Devil", Cell.CROSS)
         self._game_manager = GameRoundManager(player1, player2)
         self._field_widget = GameFieldView(self._game_manager.field)
 
+        self._height = self._game_manager.field.height * CELL_SIZE
+        self._width = self._game_manager.field.width * CELL_SIZE + 200
+        self._screen = pg.display.set_mode((self._width, self._height))
+        self._screen.fill('gray')
+        self._title = 'XO OOP'
+        pg.display.set_caption(self._title)
+
     def main_loop(self):
         """
 
         """
         clock = pg.time.Clock()
+
         finished = False
         while not finished:
+
             for event in pg.event.get():
+
                 if event.type == pg.QUIT:
                     finished = True
+
                 elif event.type == pg.MOUSEBUTTONDOWN:
-                    x, y = event.x, event.y
+                    x, y = pg.mouse.get_pos()
+
                     if self._field_widget.check_coords(x, y):
+
                         i, j = self._field_widget.get_coords(x, y)
+
                         self._game_manager.handle_click(i, j)
 
             pg.display.flip()
