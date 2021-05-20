9619522ca9475ba9ec7a82713c05d154bfcdf406
artemschirov <artemschirov@gmail.com>
2021-05-21 01:52:21 +0300

    Add test

--- lab0_tests/fib.py
+++ lab0_tests/fib.py
@@ -1,5 +1,20 @@
 def fib(n):
+    """
+    Calculates fibonacci numbers
+
+    >>> fib(1)
+    1
+    >>> fib(10)
+    55
+    >>> fib(-1)
+    None
+    """
     if n < 2:
         return n
     else:
         return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ lab0_tests/fib_test.py
@@ -0,0 +1,15 @@
+import fib
+
+all_correct = True
+
+for n, answer in [(0, 0), (1, 1), (2, 1), (5, 10)]:
+    result = fib.fib(n)
+    correct = (result == answer)
+    if not correct:
+        print('Test case failed:', n, result, '!=', answer)
+    all_correct &= correct
+
+if all_correct:
+    print('Testing: OK')
+else:
+    print('Testing fib(): Failed')
